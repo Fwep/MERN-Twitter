2757197fcb466bf0d2cbcf9c64db4b6916a9c10a
Taran Cacacho <tarancacacho@gmail.com>
2019-01-06 14:09:34 -0800

    Refactor login and register routes to use Validator modules

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,45 +5,63 @@ const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys") // To sign our web tokens
 const passport = require("passport");
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Route to register a user
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email })
-    .then(user => {
-      if (user) {
-        // Throw a 400 error if email exists already
-        return res.status(400).json({email: "Woops! An account already exists with this address!"});
-      } else {
-        // Otherwise create a new user
-        const newUser = new User({
-          handle: req.body.handle,
-          email: req.body.email,
-          password: req.body.password
-        });
-
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err))
-          })
+  .then(user => {
+    if (user) {
+      // Use the validations to send the error
+      errors.email = "Woops! An account with that email already exists!";
+      return res.status(400).json(errors);
+    } else {
+      // Otherwise create a new user
+      const newUser = new User({
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password
+      });
+      
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err))
         })
-      }
-    })
+      })
+    }
+  })
 })
 
 // Route to login a user
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  
+  
   const email = req.body.email;
   const password = req.body.password;
 
   User.findOne({email})
     .then(user => {
       if (!user) {
-        return res.status(404).json({email: "This user does not exist"});
+        // Use the validations to send the error
+        errors.email = "User not found";
+        return res.status(404).json(errors);
       }
 
       bcrypt.compare(password, user.password)
