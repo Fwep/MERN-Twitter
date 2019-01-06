620a7a4509e283280a818026e46b16778927aec6
Taran Cacacho <tarancacacho@gmail.com>
2019-01-06 00:09:37 -0800

    Add users and tweets API endpoints

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ mongoose
   .then(() => console.log("Right on! Connected to MongoDB successfully."))
   .catch(err => console.log(err))
 
-// Setup basic route
-app.get("/", (req, res) => res.send("Hello World"));
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+// Import body-parser to parse JSON we send to frontend
+const bodyParser = require('body-parser');
 
 // Specify which port to run on (process.env.PORT is for Heroku)
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// Setup basic route. N.B. the callback for every
+// Express route requires a request and response as arguments
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
