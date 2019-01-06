3f8f72eddaf9bc5ec660ae9106d1a70d51edfd95
Taran Cacacho <tarancacacho@gmail.com>
2019-01-06 13:19:21 -0800

    Fix router errors
    The user routes were not being shown correctly, because the
    body-parser middleware was being called after passing in the user and
    tweets routes to 'app'.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Create a new Express server
 const mongoose = require('mongoose');
+// Create a new Express server
 const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
@@ -12,7 +12,10 @@ mongoose
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 // Import body-parser to parse JSON we send to frontend
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const passport = require("passport");
+require('./config/passport')(passport)
+
 
 // Specify which port to run on (process.env.PORT is for Heroku)
 const port = process.env.PORT || 5000;
@@ -21,8 +24,8 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 // Setup basic route. N.B. the callback for every
 // Express route requires a request and response as arguments
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({ extended: false })); // Tell app to respond from requests from other software (i.e. Postman)
+app.use(bodyParser.json()); // Tell app to respond to JSON requests
+app.use(passport.initialize()); // Middleware for passport
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
