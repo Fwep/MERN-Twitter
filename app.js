59c52365291d3c9c0ac07f8a4fc3c4bfd266eb61
Taran Cacacho <tarancacacho@gmail.com>
2019-01-05 23:32:07 -0800

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // Create a new Express server
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const db = require('./config/keys').mongoURI;
+// Connect to MongoDB using Mongoose
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Right on! Connected to MongoDB successfully."))
+  .catch(err => console.log(err))
+
+// Setup basic route
+app.get("/", (req, res) => res.send("Hello World"));
+
+// Specify which port to run on (process.env.PORT is for Heroku)
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
