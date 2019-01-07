9bcf11470020e27ee3d08d843fee3cc34e8d5670
Taran Cacacho <tarancacacho@gmail.com>
2019-01-06 23:31:57 -0800

    Fix typo (change 'status' to 'str')

--- validation/valid-text.js
+++ validation/valid-text.js
@@ -1,6 +1,6 @@
 // Custom function to check wheter a given string consists of valid input
 const validText = str => {
-  return typeof str === "string" && status.trim().length > 0;
+  return typeof str === "string" && str.trim().length > 0;
 }
 
 module.exports = validText;
